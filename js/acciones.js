aa7e7495754c90419c8b64f09fc43ad716bae016
alumno305ecb16 <scorpions130295@gmail.com>
2014-12-09 13:06:41 -0600

    8

--- js/acciones.js
+++ js/acciones.js
@@ -5,13 +5,13 @@ $('#principal').height($('#page1').height());
 		
 		audio=window.plugins.LowLatencyAudio;
 		
-		audio.preloadFX('do','audio/DO.mp3', function(){},function(e){alert('Error'+e);});
-		audio.preloadFX('re','audio/RE.mp3', function(){},function(e){alert('Error'+e);});
-		audio.preloadFX('mi','audio/MI.mp3', function(){},function(e){alert('Error'+e);});
-		audio.preloadFX('fa','audio/FA.mp3', function(){},function(e){alert('Error'+e);});
-		audio.preloadFX('sol','audio/SOL.mp3', function(){},function(e){alert('Error'+e);});
-		audio.preloadFX('la','audio/LA.mp3', function(){},function(e){alert('Error'+e);});
-		audio.preloadFX('si','audio/SI.mp3', function(){},function(e){alert('Error'+e);});
+		audio.preloadFX('do','audio/DO.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('re','audio/RE.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('mi','audio/MI.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('fa','audio/FA.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('sol','audio/SOL.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('la','audio/LA.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('si','audio/SI.mp3', function(){},function(e){alert('Error '+e);});
 		
 		$('.nota').bind('touchstart',function(){
 			$(this).addClass('tocada');
